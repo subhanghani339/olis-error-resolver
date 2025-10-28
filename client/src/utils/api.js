5d9d1ac7f0fe2bb4a435e1fbb6863a62a52d1326
muhammad-subhan <subhanghani339@gmail.com>
2025-10-29 04:30:38 +0500

    baseURL updated

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
